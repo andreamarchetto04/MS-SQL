99a517793aae2b36318a9e910e85450426daadee
Andrea Marchetto <72484739+andreamarchetto04@users.noreply.github.com>
2022-10-19 13:23:11 +0200

    tabella materia

--- Script-7.sql
+++ Script-7.sql
@@ -24,5 +24,25 @@ CREATE TABLE student(
 );
 
 INSERT INTO student(id, nome, cognome, classe, sezione)
-SELECT DISTINCT id, nome, cognome, classe, sezione 
+SELECT DISTINCT id, nome, cognome, classe, sezione /* distinct mi prende le cose una volta*/
 FROM taccuino_tb;
+
+CREATE TABLE subject(
+	id INT IDENTITY(1, 1) PRIMARY KEY,
+	description NVARCHAR (20) NOT NULL,
+);
+
+INSERT INTO subject(description)
+SELECT DISTINCT materia
+FROM taccuino_tb;
+
+CREATE TABLE teacher(
+	id INT IDENTITY(1, 1) PRIMARY KEY,
+	nome NVARCHAR (30) NOT NULL,
+	cognome NVARCHAR (30) NOT NULL,
+	id_materia SMALLINT NOT NULL,
+	classe NVARCHAR(1) NOT NULL,
+	sezione NVARCHAR(1) NOT NULL,
+);
+
+
