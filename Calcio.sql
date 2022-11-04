0b1a353decbb7285371d85fadf591ca9081246f3
Andrea Marchetto <72484739+andreamarchetto04@users.noreply.github.com>
2022-11-04 11:21:21 +0100

    Add files via upload

--- Calcio.sql
+++ Calcio.sql
@@ -1,1 +1,21 @@
-CREATE DATABASE calcio
+CREATE DATABASE campionato;
+USE campionato;
+
+CREATE TABLE person(
+	id INT IDENTITY(1, 1) PRIMARY KEY,
+	name NVARCHAR(40) NOT NULL,
+	surname NVARCHAR(40) NOT NULL,
+	id_role INT FOREIGN KEY REFERENCES p_role(id),
+	id_society INT FOREIGN KEY REFERENCES society(id),
+)
+
+CREATE TABLE society(
+	id INT IDENTITY(1, 1) PRIMARY KEY,
+	description NVARCHAR (50) NOT NULL,
+)
+
+CREATE TABLE p_role(
+	id INT IDENTITY(1, 1) PRIMARY KEY,
+	description NVARCHAR (50) NOT NULL,
+)
+
